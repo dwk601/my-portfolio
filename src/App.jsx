1034cea38ce71fda40747ab79a5b9fb0bb9e63c5
Kim <10829539@uvu.edu>
2023-03-19 14:30:57 -0600

    added typing animation and fade in animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/header/header";
 import AboutMe from "./components/aboutme/aboutme";
 import Skills from "./components/skills/skills";
@@ -6,24 +6,72 @@ import Projects from "./components/projects/projects";
 import Education from "./components/education/education";
 import Contact from "./components/contact/contact";
 import "./App.css";
+import anime from "animejs";
+
+const animateOnScroll = (scrollDirection) => {
+  const sections = document.querySelectorAll(".scroll-animation");
+  const windowHeight = window.innerHeight;
+
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const animationDirection = section.dataset.direction;
+    const isAnimated = section.dataset.animated === "true";
+
+    if (
+      sectionTop <= windowHeight * 0.75 &&
+      scrollDirection === "down" &&
+      !isAnimated
+    ) {
+      anime({
+        targets: section,
+        translateX:
+          animationDirection === "left" ? ["-100%", "0%"] : ["100%", "0%"],
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 3000,
+        complete: () => {
+          section.dataset.animated = "true";
+        },
+      });
+    }
+  });
+};
+
 
 const App = () => {
+  const [lastScrollTop, setLastScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollDirection = scrollTop > lastScrollTop ? "down" : "up";
+      setLastScrollTop(scrollTop);
+      animateOnScroll(scrollDirection);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollTop]);
+
   return (
     <div className="app-container">
       <Header />
-      <div id="about">
+      <div id="about" className="scroll-animation" data-direction="left">
         <AboutMe />
       </div>
-      <div id="skills">
+      <div id="skills" className="scroll-animation" data-direction="right">
         <Skills />
       </div>
-      <div id="projects">
+      <div id="projects" className="scroll-animation" data-direction="left">
         <Projects />
       </div>
-      <div id="education">
+      <div id="education" className="scroll-animation" data-direction="right">
         <Education />
       </div>
-      <div id="contact">
+      <div id="contact" className="scroll-animation" data-direction="left">
         <Contact />
       </div>
       {/* The rest of your sections */}
--- src/components/aboutme/aboutme.jsx
+++ src/components/aboutme/aboutme.jsx
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Box, Typography, Avatar, Grid } from '@mui/material';
+import anime from 'animejs';
 import './aboutme.css';
 
 const AboutMe = () => {
-    return (
-      <Box sx={{ padding: 4 }}>
-        <Typography variant="h5" sx={{ marginBottom: 2 }}>
-          Passionate about Data Science & Machine Learning | Computer Science Major | Full Stack Web Dev | UVU Junior
-        </Typography>
+  useEffect(() => {
+    const typingAnimation = () => {
+      const textWrapper = document.querySelector('.aboutme-text');
+      const text = textWrapper.textContent;
+      textWrapper.textContent = '';
+      const textArray = [...text];
+
+      textArray.forEach((char, index) => {
+        const span = document.createElement('span');
+        span.innerHTML = char;
+        span.style.opacity = 0;
+        textWrapper.appendChild(span);
+
+        anime({
+          targets: span,
+          opacity: [0, 1],
+          easing: 'linear',
+          duration: 75,
+          delay: 75 * index,
+          complete: () => {
+            if (index === textArray.length - 1) {
+              setTimeout(eraseAnimation, 2000);
+            }
+          },
+        });
+      });
+    };
+
+    const eraseAnimation = () => {
+      const textWrapper = document.querySelector('.aboutme-text');
+      const spanElements = textWrapper.getElementsByTagName('span');
+      const reversedSpanElements = Array.from(spanElements).reverse();
+
+      reversedSpanElements.forEach((span, index) => {
+        anime({
+          targets: span,
+          opacity: [1, 0],
+          easing: 'linear',
+          duration: 75,
+          delay: 75 * index,
+          complete: () => {
+            if (index === reversedSpanElements.length - 1) {
+              setTimeout(typingAnimation, 2000);
+            }
+          },
+        });
+      });
+    };
+
+    typingAnimation();
+  }, []);
+
+  return (
+    <Box sx={{ padding: 4 }}>
+      <Typography variant="h5" sx={{ marginBottom: 2 }} className="aboutme-text">
+        Passionate about Data Science & Machine Learning | Computer Science Major | Full Stack Web Dev | UVU Junior
+      </Typography>
         <Grid container spacing={4}>
           <Grid item xs={12} md={6}>
             <Typography variant="h4" sx={{ marginBottom: 2 }}>
